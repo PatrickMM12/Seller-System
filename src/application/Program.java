bf2f7fbf0feb66168b9b2f510ef3074713582289
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-03-27 15:07:59 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,12 @@
 package application;
+
+import model.Department;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Department d = new Department(1, "Books");
+
+        System.out.println(d);
     }
 }
