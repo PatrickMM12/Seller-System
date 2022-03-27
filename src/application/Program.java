ceeb01754320ceff3f3eb2ceb145d454c1d6d9bc
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-03-27 15:21:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
-import model.Department;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        Department d = new Department(1, "Books");
+        Department department = new Department(1, "Books");
 
-        System.out.println(d);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
     }
 }
